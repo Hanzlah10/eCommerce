c3ba91ff8f16993b360262256a44a2c04421ac0f
Hanzlah10 <hanzalasarguroh@gmail.com>
2024-06-24 15:21:27 +0530

    chore: Update app.component.ts imports to use PrimengModule instead of individual primeng modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { FloatLabelModule } from 'primeng/floatlabel';
+import { PrimengModule } from './primeng/primeng.module';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule, FloatLabelModule],
+  imports: [CommonModule, RouterOutlet, PrimengModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/primeng/primeng.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    FloatLabelModule
+  ]
+})
+export class PrimengModule { }
