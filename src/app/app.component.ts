a636a373c6ab632ad732dcfe9c7abc1325ee6765
Hanzlah10 <hanzalasarguroh@gmail.com>
2024-06-24 15:17:15 +0530

    chore: Add Tailwind CSS  and primeng configuration and update package dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, FloatLabelModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
