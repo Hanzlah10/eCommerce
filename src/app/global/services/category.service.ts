d2888b22d4449329707473e29728d2c2c5c7eb66
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2024-08-12 01:23:50 +0530

    refactor: Update category-card component HTML and CSS

--- src/app/global/services/category.service.ts
+++ src/app/global/services/category.service.ts
@@ -3,6 +3,7 @@ import { Observable, map } from "rxjs";
 import { PaginationInterface } from "../../Shared/types/category.interface";
 import { HttpClient } from "@angular/common/http";
 import { ResponseCategoryAPI } from "../../Shared/types/respose.interface";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable({
     providedIn: 'root'
@@ -13,8 +14,8 @@ export class categoryService {
     constructor(private http: HttpClient) { }
 
     getAllCategories(): Observable<PaginationInterface> {
-        let url = `http://localhost:8080/api/v1/ecommerce/categories?page=1&limit=25`
-        // let url = environment.apiUrl + `/ecommerce/categories`
+        // let url = `http://localhost:8080/api/v1/ecommerce/categories?page=1&limit=25`
+        let url = environment.apiUrl + `/ecommerce/categories?page=1&limit=25`
         return this.http.get<ResponseCategoryAPI>(url).pipe(map(res => res.data))
     }
 }
